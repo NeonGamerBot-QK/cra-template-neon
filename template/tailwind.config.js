1f389a1d7554ecdb545964df4c9f421c388b31fc
Saahil <neon@saahild.com>
2024-07-22 14:23:05 -0400

    v0.0.2

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -1,13 +1,17 @@
+import catppuccin from '@catppuccin/daisyui'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/**.tsx'
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [
     require('daisyui')
   ],
+  daisyui: {
+    themes: [
+      // You can simply select a catppuccin flavor with sane default colors
+      catppuccin("mocha", { primary: 'mauve', secondary: 'blue' }),
+    ],
+  },
 }
 
